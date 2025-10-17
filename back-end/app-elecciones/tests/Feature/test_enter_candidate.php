ec20cbfbcf3c10f86ff94e5bec73d9966d5dcb93
dayvis360 <dayvis209@gmail.com>
2025-10-17 00:13:44 -0300

    r

--- back-end/app-elecciones/tests/Feature/test_enter_candidate.php
+++ back-end/app-elecciones/tests/Feature/test_enter_candidate.php
@@ -14,7 +14,7 @@ class test_enter_candidate extends TestCase
     public function test_example(): void
     {
         $response = $this->get('/');
-
+        
         $response->assertStatus(200);
     }
 }
