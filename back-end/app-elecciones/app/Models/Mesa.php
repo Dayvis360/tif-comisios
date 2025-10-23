0c5d0fa5ea3978c4d21878cf57f77c2ca033c23b
dayvis360 <dayvis209@gmail.com>
2025-10-23 00:25:59 -0300

    modelo mesa configurado

--- back-end/app-elecciones/app/Models/Mesa.php
+++ back-end/app-elecciones/app/Models/Mesa.php
@@ -4,7 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Mesa extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'provincia_id',
+        'circuito',
+        'establecimiento',
+        'electores'
+    ];
+    public function provincia()
+    {
+        return $this->belongsTo(Provincia::class);
+    }
+    public function telegramas()
+    {
+        return $this->hasMany(Telegrama::class);
+    }
 }
