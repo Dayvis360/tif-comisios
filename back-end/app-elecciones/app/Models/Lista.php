3a4360fd0bc9cb33a7b7c5ac1365b8ff3ed96d3c
dayvis360 <dayvis209@gmail.com>
2025-10-23 00:20:59 -0300

    modelo Lista configurado junto a sus relaciones

--- back-end/app-elecciones/app/Models/Lista.php
+++ back-end/app-elecciones/app/Models/Lista.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Lista extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'alianza',
+        'cargo',
+        'provincia_id',
+    ];
+    public function provincia()
+    {
+        return $this->belongsTo(Provincia::class);
+    }
+    public function candidatos()
+    {
+        return $this->hasMany(Candidato::class);
+    }
+    public function telegramas()
+    {
+        return $this->hasMany(Telegrama::class);
+    }
 }
