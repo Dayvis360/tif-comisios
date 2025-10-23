11b2d3efc477ee2cf4a1240f423b6dd473d99e5f
dayvis360 <dayvis209@gmail.com>
2025-10-23 00:16:06 -0300

    modelo provincia configurado y relacion

--- back-end/app-elecciones/app/Models/Provincia.php
+++ back-end/app-elecciones/app/Models/Provincia.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Testing\Fluent\Concerns\Has;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Provincia extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'nombre',
+    ];
+    public function listas()
+    {
+        return $this->hasMany(Lista::class);
+    }
+    public function mesas()
+    {
+        return $this->hasMany(Mesa::class);
+    }
 }
