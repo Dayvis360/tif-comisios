579494cef313be68891549b6748964aebf401c0f
dayvis360 <dayvis209@gmail.com>
2025-10-23 00:22:55 -0300

    modelo candidato configurado

--- back-end/app-elecciones/app/Models/Candidato.php
+++ back-end/app-elecciones/app/Models/Candidato.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Candidato extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'orden_lista',
+        'lista_id',
+    ];
+    public function lista()
+    {
+        return $this->belongsTo(Lista::class);
+    }
 }
