863c28654add1f108a09fd0aa0ffa595e5ad5a46
dayvis360 <dayvis209@gmail.com>
2025-10-31 01:42:07 -0300

    telegrama update and destroy methods

--- back-end/app-elecciones/app/Http/Controllers/TelegramaController.php
+++ back-end/app-elecciones/app/Http/Controllers/TelegramaController.php
@@ -28,4 +28,30 @@ class TelegramaController extends Controller
         $telegrama = Telegrama::create($validated);
         return response()->json($telegrama->load(['mesa.provincia', 'lista']), 201);
     }
+    public function show(Request $request, $id)
+    {
+
+    }
+    public function update (Request $request, $id)
+    {
+        $validated = $request->validate([
+            'mesa_id' => 'required|exists:mesas,id',
+            'lista_id' => 'required|exists:listas,id',
+            'votos_Diputados' => 'required|integer|min:0',
+            'votos_Senadores' => 'required|integer|min:0',
+            'voto_Blancos' => 'required|integer|min:0',
+            'voto_Nulos' => 'required|integer|min:0',
+            'voto_Recurridos' => 'required|integer|min:0'
+        ]);
+
+        $telegrama = Telegrama::findOrFail($id);
+        $telegrama->update($validated);
+        return response()->json($telegrama->load(['mesa.provincia', 'lista']), 200);
+    }
+    public function destroy($id)
+    {
+        $telegrama = Telegrama::findOrFail($id);
+        $telegrama->delete();
+        return response()->json(null, 204);
+    }
 }
