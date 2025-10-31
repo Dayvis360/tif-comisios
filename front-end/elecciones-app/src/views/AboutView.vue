669fbd84bf63ba889edd95d41760b6e34a82c030
dayvis360 <dayvis209@gmail.com>
2025-10-31 02:26:46 -0300

    front-end (framework Vue.js)

---
+++ front-end/elecciones-app/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>pepe</h2>
+    <form action="">
+      <label for="name">Name:</label>
+      <input type="text" />
+      <button type="submit"></button>
+
+    </form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
