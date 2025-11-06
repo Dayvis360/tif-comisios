b6e10166af5f104bab6bcd937823ee99fec2b021
dayvis300 <dayvis209@gmail.com>
2025-11-06 19:35:17 -0300

    ejemplo mock

---
+++ back-end/app-elecciones/tests/Unit/ProvinciaTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ProvinciaTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+
+    //No existen provincias
+    public function test_example(): void
+    {   
+        $this->assertTrue(true);
+    }
+
+    
+}
