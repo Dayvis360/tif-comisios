c4feb757e56a9efa74195a6027983cc58c933ae7
alejo <tpcruzalejo@gmail.com>
2025-12-10 12:43:30 -0300

    archivo candidatotest corregido

--- back-end/app-elecciones/tests/Unit/CandidatoTest.php
+++ back-end/app-elecciones/tests/Unit/CandidatoTest.php
@@ -1,21 +1,28 @@
 <?php
-namespace Tests\Feature;
 
-use Tests\TestCase;
-use App\Models\Lista;
-use App\Models\Provincia;
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
 use App\Models\Candidato;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Repositories\CandidatoRepository;
+use Mockery;
 
-class CandidatoControllerTest extends TestCase
-{   
-    use RefreshDatabase; 
+class CandidatoTest extends TestCase
 
-    public function listar_candidatos(){
-        $provincia = Provincia::factory()->create();
-        
+{
+    public function test_nombre_vacio()
+    {
+        $repositoryMock = Mockery::mock(CandidatoRepository::class);
+        $repositoryMock->shouldReceive('existeOrdenEnLista')->andReturn(false);
 
+        $candidato = new Candidato([
+            'nombre' => '',
+            'orden_en_lista' => 1,
+            'lista_id' => 1
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+
+        $candidato->verificarQueSeaValido($repositoryMock);
     }
-
-
-    }
+}
