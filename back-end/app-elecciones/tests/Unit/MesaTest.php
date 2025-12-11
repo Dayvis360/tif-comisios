1868044ec5f1bedea431d4c9f8d4c791fe246449
dayvis360 <dayvis209@gmail.com>
2025-12-11 00:13:41 -0300

    test de mesas

--- back-end/app-elecciones/tests/Unit/MesaTest.php
+++ back-end/app-elecciones/tests/Unit/MesaTest.php
@@ -4,11 +4,11 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use App\Models\Mesa;
-use App\Repositories\MesaRepository;
 use Mockery;
-
+//use Illuminate\Foundation\Testing\RefreshDatabase;
 class MesaTest extends TestCase
 {
+    //use RefreshDatabase;
     public function tearDown(): void
     {
         Mockery::close();
@@ -45,4 +45,45 @@ class MesaTest extends TestCase
         $this->assertEquals('Escuela N°20', $mesa->establecimiento);
         $this->assertEquals(350, $mesa->electores);
     }
+
+
+    public function test_estructura_valida(){
+
+        $mesa = new Mesa([
+            'provincia_id' => 1,
+            'circuito' => 'c-123',
+            'establecimiento' => 'Escuela N°5',
+            'electores' => -2
+        ]);
+        $this->expectException(\InvalidArgumentException::class);
+        $mesa->verificarQueSeaValida(Mockery::mock('App\Repositories\MesaRepository'));
+        
+    }
+    
+
+
+    public function test_verificar_Que_Sea_Puede_Eliminar(){
+        $mesa = Mockery::spy(Mesa::class);
+    $mesa->shouldReceive('telegramas->exists')->andReturn(false);//Simula que no tiene telegramas asociados
+    
+    $mesa->verificarQueSeaPuedeEliminar();//si el res devuelve true pasa
+    $this->assertTrue(true);//espera que no lance excepción
+    }
+    
+    
+
+    public function test_obtener_descripcion_completa()
+    {
+        $mesa = new Mesa([
+            'circuito' => 'c-456',
+            'establecimiento' => 'Escuela N°15',
+            'electores' => 400
+        ]);
+
+        $descripcion = $mesa->obtenerDescripcionCompleta();
+
+        $this->assertEquals('Mesa c-456 - Escuela N°15 (400 electores)', $descripcion);
+    }
 }
+
+    
